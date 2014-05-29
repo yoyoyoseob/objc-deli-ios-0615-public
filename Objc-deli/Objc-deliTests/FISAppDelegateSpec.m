86b3054f5588dce95f3e81bc7b50a38c5f694e51
Joe Burgess <joe@flatironschool.com>
2014-05-29 07:17:45 -0400

    Deleting junk files and adding test files

---
+++ Objc-deli/Objc-deliTests/FISAppDelegateSpec.m
@@ -0,0 +1,39 @@
+//
+//  FISAppDelegateSpec.m
+//  Objc-deli
+//
+//  Created by Joe Burgess on 5/29/14.
+//  Copyright 2014 Flatiron School. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISAppDelegate.h"
+#define EXP_SHORTHAND
+#import <Expecta.h>
+
+SpecBegin(FISAppDelegate)
+
+describe(@"FISAppDelegate", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
