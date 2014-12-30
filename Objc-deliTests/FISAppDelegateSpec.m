d63b8d767566c881b727b2f2767d6831699e9337
zdrossman <zachary.drossman@gmail.com>
2014-12-30 11:13:34 -0500

    Updated tests to correctly name methods

--- Objc-deliTests/FISAppDelegateSpec.m
+++ Objc-deliTests/FISAppDelegateSpec.m
@@ -27,43 +27,43 @@ describe(@"FISAppDelegate", ^{
 
     describe(@"takeaNumberWithName method", ^{
         it(@"Should respond to the correct selector", ^{
-            expect(delegate).to.respondTo(@selector(takeANumberWithDeli:Name:));
+            expect(delegate).to.respondTo(@selector(takeANumberWithDeliLine:Name:));
         });
 
         it(@"Should return a deli with another person", ^{
-            expect([delegate takeANumberWithDeli:deliLine Name:@"Ada"]).to.equal(@[@"Ada"]);
+            expect([delegate takeANumberWithDeliLine:deliLine Name:@"Ada"]).to.equal(@[@"Ada"]);
         });
     });
 
     describe(@"nowServingWithDeli:", ^{
         it(@"Should respond to the correct selector", ^{
-            expect(delegate).to.respondTo(@selector(nowServingWithDeli:));
+            expect(delegate).to.respondTo(@selector(nowServingWithDeliLine:));
         });
 
         it(@"Should remove the person from the deli", ^{
             [deliLine addObject:@"Ada"];
             [deliLine addObject:@"Al"];
-            expect([delegate nowServingWithDeli:deliLine]).to.equal(@[@"Al"]);
+            expect([delegate nowServingWithDeliLine:deliLine]).to.equal(@[@"Al"]);
         });
 
         it(@"Should return empty array for empty deli", ^{
-            expect([delegate nowServingWithDeli:deliLine]).to.equal(@[]);
+            expect([delegate nowServingWithDeliLine:deliLine]).to.equal(@[]);
         });
     });
 
     describe(@"lineWithDeli: method", ^{
         it(@"Should respond to the correct selector", ^{
-            expect(delegate).to.respondTo(@selector(lineWithDeli:));
+            expect(delegate).to.respondTo(@selector(deliLine:));
         });
 
         it(@"Should return the appropriate line", ^{
             [deliLine addObject:@"Ada"];
             [deliLine addObject:@"Al"];
-            expect([delegate lineWithDeli:deliLine]).to.equal(@"The line is currently: 1. Ada 2. Al");
+            expect([delegate deliLine:deliLine]).to.equal(@"The line is currently: 1. Ada 2. Al");
         });
 
         it(@"Should return The line is empty if it's empty", ^{
-            expect([delegate lineWithDeli:deliLine]).to.equal(@"The line is empty");
+            expect([delegate deliLine:deliLine]).to.equal(@"The line is empty");
         });
     });
 
